080d55a819b48ba9fadf458cbb356a123c05d3fa
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-12-06 16:54:09 +0500

    Task2

--- Tyuiu.SinitsinDV.Sprint6.Task2.V8.Lib/DataService.cs
+++ Tyuiu.SinitsinDV.Sprint6.Task2.V8.Lib/DataService.cs
@@ -12,15 +12,10 @@ namespace Tyuiu.SinitsinDV.Sprint6.Task2.V8.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (x == 0)
-                {
-                    y = 0;
-                }
-                else
-                {
-                    y = Math.Round(Math.Cos(2*x) + (Math.Sin(x) / (x + 2.5)) + 2 * x, 2);
+                
+                y = Math.Round(Math.Cos(2*x) + (Math.Sin(x) / (x + 2.5)) + 2 * x, 2);
 
-                }
+                
 
                 valueArray[count] = y;
                 count++;
