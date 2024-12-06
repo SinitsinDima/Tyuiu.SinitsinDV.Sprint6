fd0219472b391a0059841562dbe94e26184aa6c7
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-12-06 21:59:22 +0500

    Task3

--- Tyuiu.SinitsinDV.Sprint6.Task3.V24/FormMain.Designer.cs
+++ Tyuiu.SinitsinDV.Sprint6.Task3.V24/FormMain.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            groupBoxTask = new GroupBox();
+            dataGridViewMatrix = new DataGridView();
+            textBoxTask = new TextBox();
+            groupBoxResult = new GroupBox();
+            buttonHelp = new Button();
+            buttonResult = new Button();
+            textBoxResult = new TextBox();
+            textBoxRes = new TextBox();
+            groupBoxTask.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMatrix).BeginInit();
+            groupBoxResult.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBoxTask
+            // 
+            groupBoxTask.Controls.Add(dataGridViewMatrix);
+            groupBoxTask.Controls.Add(textBoxTask);
+            groupBoxTask.Location = new Point(12, 12);
+            groupBoxTask.Name = "groupBoxTask";
+            groupBoxTask.Size = new Size(737, 413);
+            groupBoxTask.TabIndex = 0;
+            groupBoxTask.TabStop = false;
+            groupBoxTask.Text = "Условие";
+            // 
+            // dataGridViewMatrix
+            // 
+            dataGridViewMatrix.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewMatrix.Location = new Point(389, 19);
+            dataGridViewMatrix.Name = "dataGridViewMatrix";
+            dataGridViewMatrix.RowHeadersVisible = false;
+            dataGridViewMatrix.Size = new Size(342, 388);
+            dataGridViewMatrix.TabIndex = 0;
+            // 
+            // textBoxTask
+            // 
+            textBoxTask.Location = new Point(3, 19);
+            textBoxTask.Multiline = true;
+            textBoxTask.Name = "textBoxTask";
+            textBoxTask.ReadOnly = true;
+            textBoxTask.Size = new Size(380, 388);
+            textBoxTask.TabIndex = 0;
+            textBoxTask.Text = resources.GetString("textBoxTask.Text");
+            // 
+            // groupBoxResult
+            // 
+            groupBoxResult.Controls.Add(buttonHelp);
+            groupBoxResult.Controls.Add(buttonResult);
+            groupBoxResult.Controls.Add(textBoxResult);
+            groupBoxResult.Controls.Add(textBoxRes);
+            groupBoxResult.Location = new Point(755, 12);
+            groupBoxResult.Name = "groupBoxResult";
+            groupBoxResult.Size = new Size(244, 426);
+            groupBoxResult.TabIndex = 1;
+            groupBoxResult.TabStop = false;
+            groupBoxResult.Text = "Вывод данных";
+            // 
+            // buttonHelp
+            // 
+            buttonHelp.Location = new Point(6, 357);
+            buttonHelp.Name = "buttonHelp";
+            buttonHelp.Size = new Size(112, 50);
+            buttonHelp.TabIndex = 3;
+            buttonHelp.Text = "?";
+            buttonHelp.UseVisualStyleBackColor = true;
+            buttonHelp.Click += buttonHelp_Click;
+            // 
+            // buttonResult
+            // 
+            buttonResult.Location = new Point(124, 336);
+            buttonResult.Name = "buttonResult";
+            buttonResult.Size = new Size(114, 84);
+            buttonResult.TabIndex = 2;
+            buttonResult.Text = "Выполнить";
+            buttonResult.UseVisualStyleBackColor = true;
+            buttonResult.Click += buttonResult_Click;
+            // 
+            // textBoxResult
+            // 
+            textBoxResult.Location = new Point(6, 65);
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.ReadOnly = true;
+            textBoxResult.Size = new Size(232, 23);
+            textBoxResult.TabIndex = 1;
+            // 
+            // textBoxRes
+            // 
+            textBoxRes.Location = new Point(6, 22);
+            textBoxRes.Name = "textBoxRes";
+            textBoxRes.ReadOnly = true;
+            textBoxRes.Size = new Size(100, 23);
+            textBoxRes.TabIndex = 0;
+            textBoxRes.Text = "Результат";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1011, 450);
+            Controls.Add(groupBoxResult);
+            Controls.Add(groupBoxTask);
+            Name = "FormMain";
+            Text = "Спринт 6 | Таск 3 | Вариант 24 | Синицин Д.В.";
+            Load += FormMain_Load;
+            groupBoxTask.ResumeLayout(false);
+            groupBoxTask.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMatrix).EndInit();
+            groupBoxResult.ResumeLayout(false);
+            groupBoxResult.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBoxTask;
+        private DataGridView dataGridViewMatrix;
+        private GroupBox groupBoxResult;
+        private TextBox textBoxTask;
+        private TextBox textBoxResult;
+        private TextBox textBoxRes;
+        private Button buttonHelp;
+        private Button buttonResult;
     }
 }
--- Tyuiu.SinitsinDV.Sprint6.Task3.V24/FormMain.cs
+++ Tyuiu.SinitsinDV.Sprint6.Task3.V24/FormMain.cs
@@ -1,3 +1,4 @@
+using Tyuiu.SinitsinDV.Sprint6.Task3.V24.Lib;
 namespace Tyuiu.SinitsinDV.Sprint6.Task3.V24
 {
     public partial class FormMain : Form
@@ -5,6 +6,52 @@ namespace Tyuiu.SinitsinDV.Sprint6.Task3.V24
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+        int[,] matrix = new int[5, 5]
+        {
+                {-17, -6, 10, 5, 3},
+                {-10, -14, 10, -7, -3},
+                {-19, 9, 8, -17, -9},
+                {-19, -5, -9, -18, 14},
+                {17, 12, 11, 12, 2}
+        };
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = Convert.ToString(ds.Calculate(matrix));
+        }
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int colums = matrix.Length / rows;
+
+            dataGridViewMatrix.ColumnCount = colums;
+            dataGridViewMatrix.RowCount = rows;
+
+            for (int i = 0; i < colums; i++)
+            {
+                dataGridViewMatrix.Columns[i].Width = 25;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    dataGridViewMatrix.Rows[i].Cells[j].Value = Convert.ToString(matrix[i, j]);
+                }
+
+            }
+
+
+        }
+
+        
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 3 выполнил студент группы ПКТб-24-1 Синицин Дмитрий Владимирович", "Сообщение");
         }
     }
 }
