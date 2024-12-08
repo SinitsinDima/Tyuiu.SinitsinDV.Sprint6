1e7852025bea9ffc491a2e399166ecf2a6b8c231
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-12-08 20:28:04 +0500

    Task5

--- Tyuiu.SinitsinDV.Sprint6.Task5.V12/FormMain.Designer.cs
+++ Tyuiu.SinitsinDV.Sprint6.Task5.V12/FormMain.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            groupBoxTask = new GroupBox();
+            textBoxTask = new TextBox();
+            dataGridViewResult = new DataGridView();
+            groupBoxResult = new GroupBox();
+            chartFunction = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            buttonOK = new Button();
+            buttonFile = new Button();
+            buttonHelp = new Button();
+            groupBoxTask.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResult).BeginInit();
+            groupBoxResult.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)chartFunction).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBoxTask
+            // 
+            groupBoxTask.Controls.Add(textBoxTask);
+            groupBoxTask.Location = new Point(0, 0);
+            groupBoxTask.Name = "groupBoxTask";
+            groupBoxTask.Size = new Size(515, 91);
+            groupBoxTask.TabIndex = 0;
+            groupBoxTask.TabStop = false;
+            groupBoxTask.Text = "Условие";
+            // 
+            // textBoxTask
+            // 
+            textBoxTask.Location = new Point(3, 19);
+            textBoxTask.Multiline = true;
+            textBoxTask.Name = "textBoxTask";
+            textBoxTask.ReadOnly = true;
+            textBoxTask.Size = new Size(506, 65);
+            textBoxTask.TabIndex = 0;
+            textBoxTask.Text = resources.GetString("textBoxTask.Text");
+            // 
+            // dataGridViewResult
+            // 
+            dataGridViewResult.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewResult.Location = new Point(6, 22);
+            dataGridViewResult.Name = "dataGridViewResult";
+            dataGridViewResult.Size = new Size(174, 342);
+            dataGridViewResult.TabIndex = 1;
+            // 
+            // groupBoxResult
+            // 
+            groupBoxResult.Controls.Add(dataGridViewResult);
+            groupBoxResult.Location = new Point(0, 97);
+            groupBoxResult.Name = "groupBoxResult";
+            groupBoxResult.Size = new Size(186, 370);
+            groupBoxResult.TabIndex = 2;
+            groupBoxResult.TabStop = false;
+            groupBoxResult.Text = "Вывод данных";
+            // 
+            // chartFunction
+            // 
+            chartArea1.Name = "ChartArea1";
+            chartFunction.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chartFunction.Legends.Add(legend1);
+            chartFunction.Location = new Point(192, 119);
+            chartFunction.Name = "chartFunction";
+            series1.ChartArea = "ChartArea1";
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chartFunction.Series.Add(series1);
+            chartFunction.Size = new Size(668, 342);
+            chartFunction.TabIndex = 3;
+            chartFunction.Text = "chart1";
+            // 
+            // buttonOK
+            // 
+            buttonOK.BackColor = Color.FromArgb(0, 192, 0);
+            buttonOK.Location = new Point(521, 30);
+            buttonOK.Name = "buttonOK";
+            buttonOK.Size = new Size(92, 48);
+            buttonOK.TabIndex = 4;
+            buttonOK.Text = "Выполнить";
+            buttonOK.UseVisualStyleBackColor = false;
+            buttonOK.Click += buttonOK_Click;
+            // 
+            // buttonFile
+            // 
+            buttonFile.BackColor = Color.Cyan;
+            buttonFile.Location = new Point(629, 30);
+            buttonFile.Name = "buttonFile";
+            buttonFile.Size = new Size(111, 48);
+            buttonFile.TabIndex = 5;
+            buttonFile.Text = "Открыть файл";
+            buttonFile.UseVisualStyleBackColor = false;
+            buttonFile.Click += buttonFile_Click;
+            // 
+            // buttonHelp
+            // 
+            buttonHelp.BackColor = Color.FromArgb(192, 0, 192);
+            buttonHelp.Location = new Point(764, 24);
+            buttonHelp.Name = "buttonHelp";
+            buttonHelp.Size = new Size(75, 60);
+            buttonHelp.TabIndex = 6;
+            buttonHelp.Text = "Справка";
+            buttonHelp.UseVisualStyleBackColor = false;
+            buttonHelp.Click += buttonHelp_Click;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(872, 470);
+            Controls.Add(buttonHelp);
+            Controls.Add(buttonFile);
+            Controls.Add(buttonOK);
+            Controls.Add(chartFunction);
+            Controls.Add(groupBoxResult);
+            Controls.Add(groupBoxTask);
+            Name = "FormMain";
+            Text = "Спринт 6 | Таск 5 | Вариант 12 | Синицин Д.В.";
+            groupBoxTask.ResumeLayout(false);
+            groupBoxTask.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResult).EndInit();
+            groupBoxResult.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)chartFunction).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBoxTask;
+        private DataGridView dataGridViewResult;
+        private GroupBox groupBoxResult;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartFunction;
+        private Button buttonOK;
+        private Button buttonFile;
+        private Button buttonHelp;
+        private TextBox textBoxTask;
     }
 }
--- Tyuiu.SinitsinDV.Sprint6.Task5.V12/FormMain.cs
+++ Tyuiu.SinitsinDV.Sprint6.Task5.V12/FormMain.cs
@@ -1,3 +1,4 @@
+using Tyuiu.SinitsinDV.Sprint6.Task5.V12.Lib;
 namespace Tyuiu.SinitsinDV.Sprint6.Task5.V12
 {
     public partial class FormMain : Form
@@ -5,6 +6,50 @@ namespace Tyuiu.SinitsinDV.Sprint6.Task5.V12
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+        string path = @"c:\Users\Дмитрий\source\repos\Tyuiu.SinitsinDV.Sprint6\Tyuiu.SinitsinDV.Sprint6.Task5.V12\bin\Debug\InPutFileTask5V12.txt";
+
+
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            dataGridViewResult.ColumnCount = 2;
+            dataGridViewResult.Columns[0].Width = 20;
+            dataGridViewResult.Columns[1].Width = 50;
+
+            this.chartFunction.ChartAreas[0].AxisX.Title = "Ось X";
+            this.chartFunction.ChartAreas[0].AxisY.Title = "Ось Y";
+
+            chartFunction.Series[0].Points.Clear();
+
+
+            double[] numsMass = new double[ds.len];
+
+            numsMass = ds.LoadFromDataFile(path);
+
+            for (int i = 0; i < numsMass.Length; i++)
+            {
+                dataGridViewResult.Rows.Add(Convert.ToString(i), Convert.ToString(numsMass[i]));
+                chartFunction.Series[0].Points.AddXY(i, numsMass[i]);
+            }
+
+
+
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process txt = new System.Diagnostics.Process();
+            txt.StartInfo.FileName = "notepad.exe";
+            txt.StartInfo.Arguments = path;
+            txt.Start();
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 3 выполнил студент группы ПКТб-24-1 Синицин Дмитрий Владимирович", "Сообщение");
         }
     }
 }
