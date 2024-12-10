e5b64f06eca7b1cb55751f232a9e8dcb571cdc07
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-12-10 23:11:49 +0500

    Task7

--- Tyuiu.SinitsinDV.Sprint6.Task7.V28.Lib/DataService.cs
+++ Tyuiu.SinitsinDV.Sprint6.Task7.V28.Lib/DataService.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.SinitsinDV.Sprint6.Task7.V28.Lib
 
             
             var rowCount = lines.Length;
-            var colCount = lines[0].Split(',').Length;
+            var colCount = lines[0].Split(';').Length;
 
             
             int[,] matrix = new int[rowCount, colCount];
@@ -21,7 +21,7 @@ namespace Tyuiu.SinitsinDV.Sprint6.Task7.V28.Lib
             
             for (int i = 0; i < rowCount; i++)
             {
-                var values = lines[i].Split(',');
+                var values = lines[i].Split(';');
                 for (int j = 0; j < colCount; j++)
                 {
                     matrix[i, j] = int.Parse(values[j]);
